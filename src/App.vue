e7ada65ffc87ae3bc152ac6ecbe7736fbe1f3610
Sergio Ribeiro <info@simetriastudio.com>
2021-02-24 10:01:20 -0500

    Taskbox created

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <InboxScreen />
+  </div>
+</template>
+
+<script>
+  import store from './store';
+  import InboxScreen from './components/InboxScreen.vue';
+  export default {
+    name: 'app',
+    store,
+    components: {
+      InboxScreen,
+    },
+  };
+</script>
+
+<style>
+  @import './index.css';
+</style>
